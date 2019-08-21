c2825f7d43740b4869678389b77b086c2fe01289
DiaZzzFF <dvsh.dreamer@gmail.com>
2019-08-21 04:44:24 +0300

    Добавляет popup

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,44 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var ESC_KEYCODE = 27;
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  var myBtnShow = document.querySelector('.contacts__btn');
+  var myPopup = document.querySelector('.popup__overlay');
+  var myBtnClose = myPopup.querySelector('.popup__close');
+
+  var yourName = myPopup.querySelector('[name=name]');
+  var tel = myPopup.querySelector('[name=tel]');
+
+  var storage = '';
+
+  myBtnShow.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    myPopup.classList.add('popup__show');
+
+    if (storage) {
+      tel.value = storage;
+      yourName.focus();
+
+    } else {
+      tel.focus();
+    }
+  });
+
+  myBtnClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+
+    myPopup.classList.remove('popup__show');
+  });
+
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      if (myPopup.classList.contains('popup__show')) {
+        evt.preventDefault();
+
+        myPopup.classList.remove('popup__show');
+      }
+    }
+  });
+})();
