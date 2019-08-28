6c250192db7a4a20fd8debf73ba48b3374afe793
DiaZzzFF <dvsh.dreamer@gmail.com>
2019-08-28 05:35:31 +0300

    Добавляет правки_3

--- source/js/script.js
+++ source/js/script.js
@@ -3,8 +3,11 @@
 (function () {
   var ESC_KEYCODE = 27;
 
-  var myBtnShow = document.querySelector('.contacts__btn');
-  var myPopup = document.querySelector('.popup__overlay');
+  var myBody = document.querySelector('body');
+  var myBtnShow = myBody.querySelector('.contacts__btn');
+  var myPopup = myBody.querySelector('.popup');
+  var myOverlay = myPopup.querySelector('.popup__overlay');
+  // var myPopupBox = myPopup.querySelector('.popup__wrapper-1');
   var myBtnClose = myPopup.querySelector('.popup__close');
 
   var yourName = myPopup.querySelector('[name=name]');
@@ -12,10 +15,13 @@
 
   var storage = '';
 
+  var myBtnScrollDown = document.querySelector('.promo__scroll-down');
+
   myBtnShow.addEventListener('click', function (evt) {
     evt.preventDefault();
 
-    myPopup.classList.add('popup__show');
+    myOverlay.classList.add('popup__show');
+    myBody.classList.add('popup__no-scroll');
 
     if (storage) {
       tel.value = storage;
@@ -29,16 +35,26 @@
   myBtnClose.addEventListener('click', function (evt) {
     evt.preventDefault();
 
-    myPopup.classList.remove('popup__show');
+    myOverlay.classList.remove('popup__show');
+    myBody.classList.remove('popup__no-scroll');
   });
 
   window.addEventListener('keydown', function (evt) {
     if (evt.keyCode === ESC_KEYCODE) {
-      if (myPopup.classList.contains('popup__show')) {
+      if (myOverlay.classList.contains('popup__show')) {
         evt.preventDefault();
 
-        myPopup.classList.remove('popup__show');
+        myOverlay.classList.remove('popup__show');
+        myBody.classList.remove('popup__no-scroll');
       }
     }
+  });
+
+  myBtnScrollDown.addEventListener('click', function () {
+    $('.advantages').animatescroll();
+  });
+
+  $(document).ready(function () {
+    $('[type=tel]').mask('+7 (000) 000-00-00');
   });
 })();
